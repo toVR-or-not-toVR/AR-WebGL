82acfa5154d1cc41bfd69b60187640f5bfec52fa
Stacy <wjagency@mail.ru>
2021-12-03 08:35:43 +0800

    Initial commit

---
+++ JSmodels.js
@@ -0,0 +1,26 @@
+import * as THREE from './js/three.module.js';
+
+let camera, scene, renderer;
+let model, material, mesh;
+
+init();
+
+function init() {
+
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+	//model = new THREE.
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setAnimationLoop( animation );
+	document.body.appendChild( renderer.domElement );
+
+}
